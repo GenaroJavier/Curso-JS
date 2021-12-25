3bac57d0048425de7f671626a6762c9f6ca01525
GenaroJavier <genaro120300@gmail.com>
2021-12-25 08:50:59 -0600

    Prompt

--- js/funciones.js
+++ js/funciones.js
@@ -1,19 +1,6 @@
-/*Tipos de datos*/
-let string = 'Genaro', booleano = true, numero = 21, vacio=null; 
-const apellido = 'Perez'; 
+/*Nos permite pedir datos al usuario*/
 
-alert(typeof(string));
-alert(typeof(booleano));
-alert(typeof(numero));
-alert(typeof(vacio));
-
-
-/*El let va a ser valido unicamente dentro del bloque donde esta fue declarada.*/
-/*Undefined quiere decir que la variable existe, pero que no esta definida*/
-/*Const no se puede declarar y despues inicializar, const se tiene que incializar cuando se declara */
-/*La variable null a diferencia de undefined que quiere decir que no esta definida, para null decimos 
-que si esta definida, y que el valor que tiene es que no no tiene nada, es null*/
-/*
-    NaN
-    Not a number
-*/
+let nombre = prompt('Ingresa tu nombre (s)'); 
+let apellidos = prompt('Ingresa tu apellido (s)'); 
+let edad = prompt('Ingresa tu edad'); 
+let telefono = prompt('Ingresa tu numero'); 
