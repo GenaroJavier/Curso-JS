20547a286dce6166cf7f3d009507a238923cbd22
GenaroJavier <genaro120300@gmail.com>
2021-12-25 08:44:08 -0600

    Tipos de datos, variables

---
+++ js/funciones.js
@@ -0,0 +1,19 @@
+/*Tipos de datos*/
+let string = 'Genaro', booleano = true, numero = 21, vacio=null; 
+const apellido = 'Perez'; 
+
+alert(typeof(string));
+alert(typeof(booleano));
+alert(typeof(numero));
+alert(typeof(vacio));
+
+
+/*El let va a ser valido unicamente dentro del bloque donde esta fue declarada.*/
+/*Undefined quiere decir que la variable existe, pero que no esta definida*/
+/*Const no se puede declarar y despues inicializar, const se tiene que incializar cuando se declara */
+/*La variable null a diferencia de undefined que quiere decir que no esta definida, para null decimos 
+que si esta definida, y que el valor que tiene es que no no tiene nada, es null*/
+/*
+    NaN
+    Not a number
+*/
