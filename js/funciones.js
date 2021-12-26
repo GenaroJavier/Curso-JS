9b8e3c54af17d968efd1d8d6f180752f716181c8
GenaroJavier <genaro120300@gmail.com>
2021-12-25 21:38:01 -0600

    Concatenación

--- js/funciones.js
+++ js/funciones.js
@@ -1,61 +1,6 @@
-/*Operadores*/
+/*Concatenación*/
+let nombre = 'Genaro Javier', ap = 'Perez Lopez', edad = 21; 
 
-function uno() {
-    var num1 = 10, num2 = 2; 
-    alert(num1 == num2)
-}
+let direccion = `Mi nombre completo es: ${nombre} ${ap} y tengo ${edad} años`;
 
-function dos() {
-    var num1 = 10, num2 = 2; 
-    alert(num1 += num2)
-}
-
-function tres() {
-    var num1 = 10, num2 = 2; 
-    alert(num1 -= num2)
-}
-
-function cuatro() {
-    var num1 = 10, num2 = 2; 
-    alert(num1 *= num2)
-}
-
-function cinco() {
-    var num1 = 10, num2 = 2; 
-    alert(num1 /= num2)
-}
-
-function seis() {
-    var num1 = 10, num2 = 2; 
-    alert(num1 %= num2)
-}
-
-function siete() {
-    var num1 = 10, num2 = 2; 
-    alert(num1 **= num2)
-}
-
-function ocho() {
-    var num1 = 10, num2 = 2; 
-    alert(num1 <<= num2)
-}
-
-function nueve() {
-    var num1 = 10, num2 = 2; 
-    alert(num1 >>>= num2)
-}
-
-function diez() {
-    var num1 = 10, num2 = 2; 
-    alert(num1 &= num2)
-}
-
-function once() {
-    var num1 = 10, num2 = 2; 
-    alert(num1 ^= num2)
-}
-
-function doce() {
-    var num1 = 10, num2 = 2; 
-    alert(num1 |= num2)
-}
+alert(direccion); 
