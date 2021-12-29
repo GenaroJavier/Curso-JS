e8e960b21b1149199f2aea47fb568f0a418e1573
GenaroJavier <genaro120300@gmail.com>
2021-12-29 14:02:58 -0600

    Arrays asociativos (Objetos)

--- js/funciones.js
+++ js/funciones.js
@@ -1,4 +1,15 @@
-/*Arrays*/
-let frutas = ['manzana', 'pera', 'platano', 'durazno'];
+/*Arrays Asociativos (Objetos)*/
 
-document.write(frutas);
+let persona = {
+    nombre: 'Genaro', 
+    apP: 'Perez', 
+    apM: 'Lopez', 
+    fecNac: '12/03/00', 
+    curp: 'PELG000312HDFRPNA7'
+}
+
+document.write(`<b>Descripción:</b><br>
+Mi nombre completo es: <b class="especial">${persona.nombre} ${persona.apP} ${persona.apM}</b>,<br>
+naci el <b>${persona.fecNac}</b>,<br>
+y mi curp es: <b>${persona.curp}</b>. 
+`)
