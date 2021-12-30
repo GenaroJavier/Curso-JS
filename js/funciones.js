a822050c4dd67b3b18bc4e4e39a23ffa99ccdaf4
GenaroJavier <genaro120300@gmail.com>
2021-12-30 12:10:15 -0600

    Practica "árbolito de navidad"

--- js/funciones.js
+++ js/funciones.js
@@ -1,8 +1,15 @@
 /*while*/
 
-let cont = 0; 
+let cont = 0, ast = 1; 
+const tronquito = `| | |`
+for (let vuelta = 0; vuelta < 7; vuelta++) {
+    for (let j = 1; j <= ast; j++) {
+        document.write(' * ');
+    }
+    document.write(`<br>`)
+    ast+=2;
+}
 
-while (cont < 10) {
-    cont++;
-    document.write(cont + '<br>')
+for (let tronco = 0; tronco <= 2; tronco++){
+    document.write(`<p class='texto'>${tronquito}<p>`);
 }
