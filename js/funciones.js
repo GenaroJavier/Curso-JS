641d89b7a8dc9aa66ef666562e63d76b4b30c1f1
GenaroJavier <genaro120300@gmail.com>
2021-12-30 15:28:13 -0600

    Continue

--- js/funciones.js
+++ js/funciones.js
@@ -1,9 +1,19 @@
 /*For*/
 
-for (let tabla = 1, j=1; tabla<=10; tabla++) {
-    document.write(`<h2 class='subtitulo'>Tabla del ${tabla}<h2>`)
-    for (let i=1; i<=10; i++) {
-        document.write(`${j} X ${i} = ${i*j}<br>`)
+/*Esto es un for basico*/
+for (let i = 0; i<=10; i++) {
+    /*Lo que el continue nos permite, en pocas palabras
+    es saltar la iteracion del for, para este ejemplo 
+    realize un for basico que va desde 0 a 10, por cada
+    iteracion este la imprime en el documento de html.
+
+    Por cada iteracion nosotros comparamos, si el contador del for
+    vale 8, cuando esta condicion se cumpla, se especifica el continue
+    por ende esta iteracion se omite y pasa a la siguiente, como 
+    resultado tenemos que el numero 8 no se imprime en nuestro for.
+    */
+    if(i == 8) {
+        continue; 
     }
-    j++;
+    document.write(`${i}<br>`);
 }
