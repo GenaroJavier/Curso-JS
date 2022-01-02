60b640428d315e1d7d1ab63bf28feb2cb479fb57
GenaroJavier <genaro120300@gmail.com>
2022-01-01 19:59:27 -0600

    Funciones flecha

--- js/funciones.js
+++ js/funciones.js
@@ -1,7 +1,19 @@
-let nombre = prompt('Ingresa tu nombre'); 
+let numero1 = parseInt(prompt("Ingresa el numero 1"));
+let numero2 = parseInt(prompt("Ingresa el numero 2"));
+let nombre = prompt('Cual es tu nombre?');
 
-function saludar (nom) {
-    return `Hola ${nom}`
+//funcion flecha 
+const sumar = (num1, num2) => {
+    return (num1 + num2)
 }
 
-alert(saludar(nombre));
+alert(`El resultado es: ${sumar(numero1, numero2)}`);
+
+//funcion flecha con un parametro, cuando se utiliza un solo parametro
+//no es necesario poner los parentesis. 
+
+const saludar = nomb => {
+    return `Hola ${nombre}`;
+}
+
+alert(saludar()); 
