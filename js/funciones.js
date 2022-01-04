f6873616d52167fce9612b0407c31ef1f89eae40
GenaroJavier <genaro120300@gmail.com>
2022-01-04 00:00:34 -0600

    Historia de cofla 2.2

--- js/funciones.js
+++ js/funciones.js
@@ -1,24 +1,30 @@
-let primero = false; 
+/*Historia de cofla 2.0*/
+/*Cofla vuelve de la comisaria exausto y por lo cansado que estaba se fue a dormir
+Al otro dia comienzan las clases del ciclo básico de la universidad (Por que cofla quiere
+ser programador y se inscribio en la facultad de ingenieria para estudiar desarrollo de
+software). 
+En su curso en total son 19 alumnos, pero surgio un problema que complico un poco a la facultad:
+se rompoio el sistema de registro de asistencias y durante los proximos 30 dias no se podrán
+hacer registros de datos de ningun tipo, por ende las clases no podrán comenzar hasta que 
+este solucionado
 
-const VerificarEntrada = (hraEntrada) => {
-    let edad = parseInt(prompt('Introduce tu edad'));
+    - Crear un mini sistema que nos permita registrar los alumnos que estan presentes (P) 
+    y ausentes (A) en clase. 
     
-    if (edad >= 18) {
-        if (primero == false && hraEntrada >= 2) {
-            alert('Bienvenido tu entrada es gratis'); 
-            primero = true; 
-        } else {
-            alert('Bienvenido por favor pasa a pagar tu entrada'); 
-        }
-    }
-    else {
-        alert('Acceso denegado, necesitas ser mayor de edad')
-    }
-}
+    - Pasados los 30 dias mostrar la situación final de todos los alumnos (Nro total de presentes
+    y ausentes).
 
-VerificarEntrada(1)
-VerificarEntrada(3)
-VerificarEntrada(0.2)
-VerificarEntrada(1)
-VerificarEntrada(0.3)
-VerificarEntrada(3)
+    - Se puede tener un minimo de 10% de ausencias por semestre, si se tienen mas aclarar que 
+    esta reprobado.
+*/
+
+let asistencias = 30, faltas = 0; 
+
+for (let i = 0; i <= 18; i++) {
+    for (let j = 0; j<=29; j++) {
+        faltas = Math.floor(Math.random() * (5 - 1)) + 1;
+    }
+    if (faltas > 3) document.write(`Alumno: 00${i+1} Asistencias: ${asistencias-faltas}, Faltas: ${faltas} <b>REPROBADO</b><br>`);     
+        else document.write(`Alumno: 00${i+1} Asistencias: ${asistencias-faltas}, Faltas: ${faltas} <br>`);
+    faltas = 0; 
+}
