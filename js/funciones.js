cabd2dccbf5b52e9ec1058441ca75ff58d050b59
GenaroJavier <genaro120300@gmail.com>
2022-01-05 23:52:21 -0600

    Historia de cofla 2.3 (Calculadora)

--- js/funciones.js
+++ js/funciones.js
@@ -1,30 +1,136 @@
-/*Historia de cofla 2.0*/
-/*Cofla vuelve de la comisaria exausto y por lo cansado que estaba se fue a dormir
-Al otro dia comienzan las clases del ciclo básico de la universidad (Por que cofla quiere
-ser programador y se inscribio en la facultad de ingenieria para estudiar desarrollo de
-software). 
-En su curso en total son 19 alumnos, pero surgio un problema que complico un poco a la facultad:
-se rompoio el sistema de registro de asistencias y durante los proximos 30 dias no se podrán
-hacer registros de datos de ningun tipo, por ende las clases no podrán comenzar hasta que 
-este solucionado
+/*Historia de cofla 3.0*/
+//Calculadora
 
-    - Crear un mini sistema que nos permita registrar los alumnos que estan presentes (P) 
-    y ausentes (A) en clase. 
-    
-    - Pasados los 30 dias mostrar la situación final de todos los alumnos (Nro total de presentes
-    y ausentes).
+var num1, resultado, operacion; 
 
-    - Se puede tener un minimo de 10% de ausencias por semestre, si se tienen mas aclarar que 
-    esta reprobado.
-*/
+const uno = () => {
+    let dato = document.getElementById('screen').value; 
+    document.getElementById('screen').value = dato+1;
+}
 
-let asistencias = 30, faltas = 0; 
+const dos = () => {
+    let dato = document.getElementById('screen').value; 
+    document.getElementById('screen').value = dato+2;
+}
 
-for (let i = 0; i <= 18; i++) {
-    for (let j = 0; j<=29; j++) {
-        faltas = Math.floor(Math.random() * (5 - 1)) + 1;
+const tres = () => {
+    let dato = document.getElementById('screen').value; 
+    document.getElementById('screen').value = dato+3;
+}
+
+const cuatro = () => {
+    let dato = document.getElementById('screen').value; 
+    document.getElementById('screen').value = dato+4;
+}
+
+const cinco = () => {
+    let dato = document.getElementById('screen').value; 
+    document.getElementById('screen').value = dato+5;
+}
+
+const seis = () => {
+    let dato = document.getElementById('screen').value; 
+    document.getElementById('screen').value = dato+6;
+}
+
+const siete = () => {
+    let dato = document.getElementById('screen').value; 
+    document.getElementById('screen').value = dato+7;
+}
+
+const ocho = () => {
+    let dato = document.getElementById('screen').value; 
+    document.getElementById('screen').value = dato+8;
+}
+
+const nueve = () => {
+    let dato = document.getElementById('screen').value; 
+    document.getElementById('screen').value = dato+9;
+}
+
+const cero = () => {
+    let dato = document.getElementById('screen').value; 
+    document.getElementById('screen').value = dato+0;
+}
+
+const punto = () => {
+    let dato = document.getElementById('screen').value; 
+    let punto = false; 
+    for (let letra of dato) {
+        if (letra == '.') {
+            punto = true; 
+            alert('Solo puedes usar un punto decimal')
+            break; 
+        }
     }
-    if (faltas > 3) document.write(`Alumno: 00${i+1} Asistencias: ${asistencias-faltas}, Faltas: ${faltas} <b>REPROBADO</b><br>`);     
-        else document.write(`Alumno: 00${i+1} Asistencias: ${asistencias-faltas}, Faltas: ${faltas} <br>`);
-    faltas = 0; 
+    if (punto == false) document.getElementById('screen').value = dato+'.';
+}
+
+const borrar = () =>{
+    document.getElementById('screen').value = '';
+}
+
+const pedirnumero = () => {
+    return (document.getElementById('screen').value); 
+}
+
+const dividir = () => {
+    num1 = parseFloat(pedirnumero()); 
+    borrar();
+    operacion = 1; 
+}
+
+const residuo = () => {
+    num1 = parseFloat(pedirnumero()); 
+    borrar();
+    operacion = 2; 
+}
+
+const multiplicacion = () => {
+    num1 = parseFloat(pedirnumero()); 
+    borrar();
+    operacion = 3; 
+}
+
+const resta = () => {
+    num1 = parseFloat(pedirnumero()); 
+    borrar();
+    operacion = 4; 
+}
+
+const suma = () => {
+    num1 = parseFloat(pedirnumero()); 
+    borrar();
+    operacion = 5; 
+}
+
+const potencia = () => {
+    num1 = parseFloat(pedirnumero()); 
+    borrar();
+    operacion = 6; 
+}
+
+const imprimir = () => {
+    let num2 = parseFloat(pedirnumero()); 
+
+    switch (operacion) {
+        case 1:
+            document.getElementById('screen').value = (num1/num2);       
+            break;
+        case 2:
+            document.getElementById('screen').value = (num1%num2);       
+            break;
+        case 3:
+            document.getElementById('screen').value = (num1*num2);       
+            break;
+        case 4:
+            document.getElementById('screen').value = (num1-num2);       
+            break;
+        case 5:
+            document.getElementById('screen').value = (num1+num2);       
+            break;
+        case 6:
+            document.getElementById('screen').value = (num1**num2);       
+            break;
+    }
 }
