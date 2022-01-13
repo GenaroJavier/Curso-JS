e8b1360a573ca8068c338a68ab987ceebae568b3
GenaroJavier <genaro120300@gmail.com>
2022-01-12 23:55:19 -0600

    Metodos strings

--- js/funciones.js
+++ js/funciones.js
@@ -1,60 +1,63 @@
-class Aplicacion {
-     constructor (numDescargas, puntuacion, peso) {
-        this.numDescargas = numDescargas; 
-        this.puntuacion = puntuacion; 
-        this.peso = peso;
-        this.Instalacion = false;
-        this.Ejecucion = false; 
-     }
+//Metodos cadenas 
+let cadena = 'cadena de prueba'; 
+let cadena2 = 'prueba'; 
 
-    Instalar () {
-         if (this.Instalacion == false) {
-             this.Instalacion = true; 
-             alert('Instalando aplicación');
-         } else alert('Aplicación instalada');
-     }
+//concatena dos strings
+    alert(cadena.concat(cadena2)); 
 
-    Abrir () {
-        if (this.Ejecucion == false && this.Instalacion == true) {
-            this.Ejecucion = true; 
-            alert('Abriendo aplicación');
-        } else this.Instalacion == false ? alert('Esta aplicación no esta instalada') : 
-        alert('Abriendo aplicación');
-    }
+//Si una cadena comienza con los caracteres de otro cadena, devuelve true, sino devuelve false, cabe menciaonar que este metodo es muy estricto.
+    alert(cadena.startsWith(cadena2)); 
 
-    Cerrar () {
-        if (this.Ejecucion == true) {
-            this.Ejecucion = false; 
-            alert('Cerrando aplicación');
-        } else alert('Esta aplicación no se esta ejecutando');
-    }
+//Si una cadena termina con los caracteres de otro cadena, devuelve true, sino devuelve false, cabe menciaonar que este metodo es muy estricto.
+    alert(cadena.endsWith(cadena2)); 
 
-    Desinstalar () {
-        if (this.Instalacion == true) {
-            this.Instalacion = false; 
-            alert('Desinstalando aplicación');
-        } else alert('Esta aplicación no esta instalada en el dispositivo');
-    }
+//Si una cadena puede encontrarse dentro de otra cadena, devulve true, si no devuelve false. 
+    alert(cadena.includes(cadena2));
 
-    appInfo() {
-        document.write(`
-        <br>
-        <b>Numero de descargas:</b> ${this.numDescargas} <br>
-        <b>Puntuación:</b> ${this.puntuacion} <br>
-        <b>Peso:</b> ${this.peso} <br>
-        `); 
-    }
-}
+//Devuelve el indice del primer caracter de la cadena, si no existe, devuelve -1
+    alert(cadena.indexOf(cadena2)); 
 
-const verApps = () => {
-    let numApps = parseInt(prompt('Cuantas apps deseas ver?')); 
-    for (let aplicación = 0; aplicación <=numApps; aplicación++) {
-        const app = new Aplicacion(
-            Math.floor(Math.random() * (2000 - 1) + 1),
-            Math.floor(Math.random() * (5 - 1) + 1), 
-            `${Math.floor((Math.random() * (1000 - 40) + 40))}mb`);
-        app.appInfo();
-    }
-}
+//Devuelve el indice de la ultima posicion del  caracter de la cadena, si no existe, devuelve -1
+    alert(cadena.lastIndexOf(cadena2)); 
 
-const Aplicacion1 = new Aplicacion ('3212', '4', '1200mb'); 
+//Rellenar cadena al principio con los caracteres deseados. 
+    alert(cadena.padStart(22, '*'));
+
+
+//Rellenar cadena al final con los caracteres deseados. 
+    alert(cadena.padEnd(22, '*'));
+
+//Repetir una cadena. 
+    alert(cadena.repeat(3)); 
+
+/*Divide la cadena de acuerdo a como le especifiquemos nosotros, por ejemplo
+tenemos la oracion 'Hola-Genaro-Javier'
+si nosotros especificamos que queremos que la oracion se divida en un arreglo y que en cada posicion 
+del arreglo este la palabra antes del - , tambien podemos especificar el espacio como en el siguiente ejemplo.
+*/
+    let res = cadena.split(' '); 
+    alert(res[0]); 
+    alert(res[1]); 
+    alert(res[2]); 
+
+//Nos retorna un pedazo de la cadena, segun nosotros le especifiquemos. 
+    alert(cadena.substring(0,3));
+
+//Retorna la cadena en minusculas
+    alert(cadena.toLowerCase());
+
+//Retorna la cadena en mayusculas
+    alert(cadena.toUpperCase());
+
+//convierte un dato en sting. 
+    let edad = 31; 
+    alert(typeof(edad.toString())); 
+
+//Elimina los espacios en blanco al principio o final de la oracion
+    alert(cadena.trim());
+
+//Elimina los espacios en blanco al principio del string;
+    alert(cadena.trimStart());
+
+//Elimina los espacios en blanco al final del string;
+    alert(cadena.trimEnd());
