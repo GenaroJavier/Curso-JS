c8e741fb7e5922e46ea460aed7729a6ff4495115
Genaro Javier <genaro120300@gmail.com>
2022-04-18 15:05:41 -0500

    Obtencion de elementos

--- js/funciones.js
+++ js/funciones.js
@@ -1,41 +1,17 @@
 /**
- * Clases, classList y metodos de classList 
+ * Obtención y modificación de elementos. 
  */
 
+const titulo = document.querySelector('.tittle'); 
 
-const titulo = document.querySelector(".tittle"); 
+//Devuelve el texto de cualquier nodo
+let res1 = titulo.textContent; 
+alert(res1); 
 
-//Añade una clase 
-titulo.classList.add('tittle-red'); 
+//Devuelve el contenido html de un elemento 
+let res2 = titulo.innerHTML; 
+alert(res2); 
 
-//Remueve una clase  
-//titulo.classList.remove('tittle'); 
-
-//Te deveulve el valor de la clase que especifiques
-document.write(titulo.classList.item(0));
-
-
-//Verifica si la clase especificada esta en el objeto, devolviendo true o false.
-document.write('<br>' + titulo.classList.contains('tittle'));
-document.write('<br>' + titulo.classList.contains('tit'));
-
-
-/**
- * Lo que hace este metodo es similar al de arriba pero por ejemplo
-    * en el caso de que si exista la clase, la elimina
-    * al contrario si no existe la clase la agrega
-    * 
-    * Podemos especificar un valor booleano ejemplo: 
-        * titulo.classList.toggle('titulo-principal', true); 
-    * de esta forma estamos "Forzando" a que se ejecute una accion, por ejemplo
-    * si le ponemos true y la clase no existe la va a agregar, si existe ahi la va a dejer
-    * de otro modo si le ponemos false, clase existe, la elimina si no existe asi la va a dejar.
- */
-
-//No existe
-titulo.classList.toggle('titulo-principal'); 
-//Existe
-titulo.classList.toggle('tittle-red'); 
-
-//Remplaza una clase por otra
-titulo.classList.replace('titulo-principal','tittle-principal'); 
+//devuelve el codigo html completo del elemento
+let res3 = titulo.outerHTML; 
+alert(res3); 
