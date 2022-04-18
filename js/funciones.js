4176ff2f60b016963687c2660178641b37544a66
Genaro Javier <genaro120300@gmail.com>
2022-04-17 19:41:29 -0500

    Atributos input

--- js/funciones.js
+++ js/funciones.js
@@ -1,30 +1,30 @@
-/**
- * Atributos globales
- */
+//Atributos input 
 
-const titulo = document.getElementById('titulo'); 
+const nombre = document.getElementById('nombre');
 
-//Permite hacer un elemento editable 
-//titulo.setAttribute('contentEditable', true);  
+const imprimir = () => {
+    //Obtienes el valor del input
+    document.write(nombre.value);
+}
+
+const cambiarTipo = () => {
+    //Cambias el tipo de input 
+    nombre.type = "number"; 
+}
+
+const seleccionarArchivo = () => {
+    //Cambias el tipo de input 
+    nombre.type = "file"; 
+    //Especificas el tipo de archivo que debe ser introducido
+    nombre.accept = "image/gif"
+}
+
+    //Verifica que la longitud de una entrada de un form sea la minima especificada  
+    nombre.minLength = 5; 
+    nombre.placeholder = "Ingresa tu nombre";
+    //Solo basta con especificar algo entre las comillas para decir que requerido este campo, si no se especifica no aparece la notificación 
+    nombre.required = 'true'; 
 
 
 
-//Cambia la direccion del texto 
-/**
- * rtl (derecha - izquiera)
- * ltr (izquierda - derecha)
- */
-
-//titulo.setAttribute('dir', 'rtl'); 
-
-//Muestra o oculta un elemento
-//titulo.setAttribute('hidden', true); 
-
-//Nos permite modificar el tabindex de un elemento
-// titulo.setAttribute('tabindex', '0'); 
-
-//Nos permite especificar un titulo al elemento
-//titulo.setAttribute('title', 'Esto es un titulo'); 
-
-
-//Aun quedan mas atributos globales, se recomienda seguir aprendiendo de la vida y programacion. 
+//No hace falta utilizar el setAttribute 
