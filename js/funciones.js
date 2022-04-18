fb586cf941876d7dc9d549aef454c97fa408425a
Genaro Javier <genaro120300@gmail.com>
2022-04-17 19:53:07 -0500

    Atributo style

--- js/funciones.js
+++ js/funciones.js
@@ -1,30 +1,12 @@
-//Atributos input 
+/**
+ * Attribute style para modificar los estilos
+ */
 
-const nombre = document.getElementById('nombre');
+ const titulo = document.getElementById('tittle-2'); 
 
-const imprimir = () => {
-    //Obtienes el valor del input
-    document.write(nombre.value);
-}
-
-const cambiarTipo = () => {
-    //Cambias el tipo de input 
-    nombre.type = "number"; 
-}
-
-const seleccionarArchivo = () => {
-    //Cambias el tipo de input 
-    nombre.type = "file"; 
-    //Especificas el tipo de archivo que debe ser introducido
-    nombre.accept = "image/gif"
-}
-
-    //Verifica que la longitud de una entrada de un form sea la minima especificada  
-    nombre.minLength = 5; 
-    nombre.placeholder = "Ingresa tu nombre";
-    //Solo basta con especificar algo entre las comillas para decir que requerido este campo, si no se especifica no aparece la notificación 
-    nombre.required = 'true'; 
-
-
-
-//No hace falta utilizar el setAttribute 
+ titulo.style.color = 'green';
+ titulo.style.background = 'gray';
+ titulo.style.padding = '1rem';
+ titulo.style.textAlign = 'center';
+ titulo.style.fontFamily = 'Helvetica';
+ titulo.style.fontWeight = '600';
