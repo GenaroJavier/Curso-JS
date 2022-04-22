cb9b62137b9f280b154dc0e0949d521a64bf6327
Genaro Javier <genaro120300@gmail.com>
2022-04-21 22:55:36 -0500

    Creación de elementos

--- js/funciones.js
+++ js/funciones.js
@@ -1,17 +1,40 @@
 /**
- * Obtención y modificación de elementos. 
+ * Creación de elementos
  */
 
-const titulo = document.querySelector('.tittle'); 
+const contenedor = document.querySelector(".contenedor"); 
 
-//Devuelve el texto de cualquier nodo
-let res1 = titulo.textContent; 
-alert(res1); 
+//Crea un elemento 
+const item = document.createElement("LI");
 
-//Devuelve el contenido html de un elemento 
-let res2 = titulo.innerHTML; 
-alert(res2); 
+//Creas un nodo de tipo texto
+const texto_del_item = document.createTextNode("Esto es un item de la lista");
 
-//devuelve el codigo html completo del elemento
-let res3 = titulo.outerHTML; 
-alert(res3); 
+//(appendChild) Es un metodo que se aplica al padre pero se toma como parametro al hijo
+item.appendChild(texto_del_item);
+contenedor.appendChild(item);
+
+console.log(item); 
+
+/**
+ * Tambien se puede hacer de la siguiente forma
+ * (este metodo es poco practico)
+ */
+ const item2 = document.createElement("LI");
+ item2.innerHTML = "Esto es un 2 elemento"; 
+ contenedor.appendChild(item2);
+
+ /**
+  * Con esta funcion podemos ahorranos todos esos recursos 
+  * desperdiciados al momento de crear elementos con el dom 
+  */
+
+ const fragmento = document.createDocumentFragment("LI");
+
+ for (let x = 0; x<10; x++) {
+    const item3 = document.createElement("LI");
+    item3.innerHTML = "Item de la lista: " + (parseInt(x)+1); 
+    fragmento.appendChild(item3);
+ }
+
+ contenedor.appendChild(fragmento);
