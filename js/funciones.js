72f0df4cff61dba13390bbec589d18c784156be2
Genaro Javier <genaro120300@gmail.com>
2022-05-05 17:47:56 -0500

    Open, Close, Closed, Stop, Alert, Print, Prompt y Confirm

--- js/funciones.js
+++ js/funciones.js
@@ -3,29 +3,28 @@
  * Historia de cofla. 
  */
 
-const contenedor = document.querySelector('.contenedor');
+// const url = "http://youtube.com/"; 
 
-const CrearLlave = (nombre, modelo, precio) => {
-    img = '<img class="llave" src="llave.png" alt="">';
-    nombre = `<h2>${nombre}</h2>`;
-    modelo = `<h4>Modelo: ${modelo}</h4>`; 
-    precio = `<p>Precio: $${precio}</p>`;
+//Abrir una ventana
+// const ventana = window.open(url); 
 
-    return [img, nombre, modelo, precio]; 
-}
+//Cerrar una ventana
+// window.close(ventana);
 
-let documentFragment = document.createDocumentFragment();
+//Indica si la ventana referenciada esta cerrada o no
+// console.log(ventana.closed); 
 
-for (let index = 0; index < 20; index++) {
-    modeloRandom = Math.round(Math.random()*1000);
-    precioRandom = Math.round(Math.random()*10*30);
-    let llave = CrearLlave(`llave ${index+1}`, modeloRandom, precioRandom); 
+//Detiene que una pagina se siga cargando 
+// ventana.stop();
 
-    let div = document.createElement("DIV");
-    div.classList.add(`flex-item`);
-    div.classList.add(`flex-item-${index}`);
-    div.innerHTML = llave[0] + llave[1] + llave[2] + llave[3]; 
-    documentFragment.appendChild(div);
-}
+// alert("Una alerta comun y corriente");
 
-contenedor.appendChild(documentFragment);
+//Con esta funcion nosotros podemos imprimir la pagina gracias a js y el navegador
+// print();
+
+//Nos permite obtener un dato del usuario, todo lo que retorne sera siempre un string. 
+// prompt("dame un dato");
+
+//Muestra una ventana como la del alert pero con las opciones de si y no para que el usuario escoja, nos devuelve un true o una false. 
+// confirm("Estas seguro de seguir continuando con este curso?");
+
